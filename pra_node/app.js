4721e0ee93d2eab04a07d60cf06f1611787fdd76
GanaBBoy <ggunna96@nate.com>
2023-03-23 17:40:16 +0900

    node js

---
+++ pra_node/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const { sequelize } = require('./models');
+
+const app = express();
+app.set('port', process.env.PORT || 3001);
+app.set('view engine','html.');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터 베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join('public')));
+app.use(express.json());
