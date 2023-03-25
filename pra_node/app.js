cf41e602616b8185c14005b2531e6a5c6a25d3c7
GanaBBoy <ggunna96@nate.com>
2023-03-25 18:17:39 +0900

    nodejs(express) sns_service

--- pra_node/app.js
+++ pra_node/app.js
@@ -23,3 +23,21 @@ sequelize.sync({ force: false })
 app.use(morgan('dev'));
 app.use(express.static(path.join('public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
